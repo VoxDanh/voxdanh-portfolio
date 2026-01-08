0c0147a5214ec416a53c5d4f66477ae280e14c5d
VoxDanh <vcongdanh8686@gmail.com>
2026-01-08 20:45:15 +0700

    chore: init project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Welcome from './component/Welcome/Welcome.jsx'
+import Resume from './component/Resume/Resume.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Welcome />
+    </>
+  )
+}
+
+export default App
